849228886cc727f3f9a6d14269d8096f72851ad2
cbellom <cbellom@unal.edu.co>
2015-05-02 11:23:37 -0500

    Se arregla fisicas de colloder del gazeIndicator Se reconoce colision de objetos que se deben resaltar

---
+++ Assets/Scripts/StandOutObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandOutObject : ObjectInteractable {
+	void Awake(){
+		ObjectSelected = HanldeObjectSelected;
+	}
+
+	private void HanldeObjectSelected(){
+		Debug.Log("Change Color");
+	}
+}
