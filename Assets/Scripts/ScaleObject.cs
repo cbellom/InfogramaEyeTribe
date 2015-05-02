026773c47dad785cf384e8f8a93431b02bc9aaab
cbellom <cbellom@unal.edu.co>
2015-05-02 15:33:40 -0500

    Activar/Desactivar render de resto objetos del "mundo" al momento de escalar el target

--- Assets/Scripts/ScaleObject.cs
+++ Assets/Scripts/ScaleObject.cs
@@ -16,12 +16,15 @@ public class ScaleObject : ObjectInteractable{
 	void OnApplicationQuit() {
 		HandleObjectExited ();
 	}
+
 	private void HandleObjectSelected(){
+		InactiveFamilyObjectsOf (this.gameObject);
 		SetScaleObject ();		
 		Debug.Log("Change Shared");
 	}
 	
 	private void HandleObjectExited ()	{
+		ActiveFamilyObjectsOf (this.gameObject);
 		RestoreScaleObject ();
 		Debug.Log("Reset Shared");
 	}
@@ -30,7 +33,7 @@ public class ScaleObject : ObjectInteractable{
 		float xScale = this.gameObject.transform.localScale.x * scale;
 		float yScale = this.gameObject.transform.localScale.y * scale;
 		float zScale = this.gameObject.transform.localScale.z * scale;
-		
+
 		this.gameObject.transform.localScale = new Vector3(xScale,yScale,zScale);
 	}
 
@@ -38,4 +41,31 @@ public class ScaleObject : ObjectInteractable{
 		this.gameObject.transform.localScale = new Vector3(cachedScale.x,cachedScale.y,cachedScale.z);
 	}
 
+	private void InactiveFamilyObjectsOf(GameObject son){
+		Transform parent = son.transform.parent;
+		for (int i=0; i < parent.childCount; i++) {
+			if(son != parent.GetChild (i).gameObject){
+				parent.GetChild (i).gameObject.SetActive(false);
+			}
+		}
+		//corregir eliminacion del render
+		parent.gameObject.renderer.enabled = false;
+		if (parent.tag != "WorldModels") {
+			InactiveFamilyObjectsOf (parent.gameObject);
+		}
+	}
+
+	private void ActiveFamilyObjectsOf(GameObject son){
+		Transform parent = son.transform.parent;
+		for (int i=0; i < parent.childCount; i++) {
+			if(son != parent.GetChild (i).gameObject){
+				parent.GetChild (i).gameObject.SetActive(true);
+			}
+		}
+
+		parent.gameObject.renderer.enabled = true;
+		if (parent.tag != "WorldModels") {
+			ActiveFamilyObjectsOf (parent.gameObject);
+		}
+	}
 }
